0aaf61812b239167f510a6e2844444be0bbfc7da
Kai Schwarz <kschwarz@hexonet.net>
2018-07-05 13:00:08 +0200

    added comments

--- src/app_nosession.ts
+++ src/app_nosession.ts
@@ -4,10 +4,11 @@ import * as apiconnector from "@hexonet/ispapi-apiconnector";
 const apiclient = new apiconnector.Client();
 const socketparameters =  {
     params: {
-      entity: "1234",
-      login: "test.user",
-      pw: "test.passw0rd",
-      remoteaddr: "1.2.3.4:80",
+      entity: "1234", // OT&E system, use "54cd" for LIVE system
+      login: "test.user", // your user id, here: the OT&E demo user
+      pw: "test.passw0rd", // your user password
+      // user: "...",//can be used to work with a subuser account - optional
+      remoteaddr: "1.2.3.4:80", // optional: provide your remote ip address (use for ip filter)
     },
   };
 

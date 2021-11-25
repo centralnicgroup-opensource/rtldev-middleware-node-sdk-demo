5777ea389d63a48d8b8b190f5243fc29df5e1b91
Kai Schwarz <kschwarz@hexonet.net>
2021-11-25 11:44:04 +0100

    fix(esm): fixed module resolution (until we are at ES6 in our SDK)

--- src/app_nosession.ts
+++ src/app_nosession.ts
@@ -2,25 +2,21 @@
 
 import { APIClient } from "@hexonet/ispapi-apiconnector";
 
-async function main(): Promise<any> {
-  const cl = new APIClient();
+const cl = new APIClient();
 
-  // choose endpoint system and set credentials
-  cl.useOTESystem();
-  cl.setCredentials("test.user", "test.passw0rd");
+// choose endpoint system and set credentials
+cl.useOTESystem();
+cl.setCredentials("test.user", "test.passw0rd");
 
-  // optional: access on a subuser account
-  // cl.setUserView("...");
+// optional: access on a subuser account
+// cl.setUserView("...");
 
-  // optional: provide your remote ip address (use for ip filter)
-  cl.setRemoteIPAddress("1.2.3.4:80");
+// optional: provide your remote ip address (use for ip filter)
+cl.setRemoteIPAddress("1.2.3.4:80");
 
-  const r = await cl.request({
-    COMMAND: "StatusAccount",
-  });
-  console.dir(r.getPlain());
-  console.dir(r.getHash());
-  console.dir(r.getListHash());
-  return r;
-}
-main();
+const r = await cl.request({
+  COMMAND: "StatusAccount",
+});
+console.dir(r.getPlain());
+console.dir(r.getHash());
+console.dir(r.getListHash());
--- src/app_session.ts
+++ src/app_session.ts
@@ -2,40 +2,36 @@
 
 import { APIClient } from "@hexonet/ispapi-apiconnector";
 
-async function main(): Promise<any> {
-  const cl = new APIClient();
+const cl = new APIClient();
 
-  // choose endpoint system and set credentials
-  cl.useOTESystem();
-  cl.setCredentials("test.user", "test.passw0rd");
+// choose endpoint system and set credentials
+cl.useOTESystem();
+cl.setCredentials("test.user", "test.passw0rd");
 
-  // optional: access on a subuser account
-  // cl.setUserView("...");
+// optional: access on a subuser account
+// cl.setUserView("...");
 
-  // optional: provide your remote ip address (use for ip filter)
-  cl.setRemoteIPAddress("1.2.3.4:80");
+// optional: provide your remote ip address (use for ip filter)
+cl.setRemoteIPAddress("1.2.3.4:80");
 
-  // --- Perform Login ---
-  let r = await cl.login();
+// --- Perform Login ---
+let r = await cl.login();
+if (r.isSuccess()) {
+  console.log("LOGIN -> SUCCEEDED");
+
+  // --- Perform API requests reusing the API session ---
+  r = await cl.request({
+    COMMAND: "StatusAccount",
+  });
+  console.dir(r.getHash());
+
+  // --- Perform Logout ---
+  r = await cl.logout();
   if (r.isSuccess()) {
-    console.log("LOGIN -> SUCCEEDED");
-
-    // --- Perform API requests reusing the API session ---
-    r = await cl.request({
-      COMMAND: "StatusAccount",
-    });
-    console.dir(r.getHash());
-
-    // --- Perform Logout ---
-    r = await cl.logout();
-    if (r.isSuccess()) {
-      console.log("LOGOUT -> SUCCEEDED");
-    } else {
-      console.log("LOGOUT -> FAILED");
-    }
+    console.log("LOGOUT -> SUCCEEDED");
   } else {
-    console.log("LOGIN -> FAILED");
+    console.log("LOGOUT -> FAILED");
   }
-  return r;
+} else {
+  console.log("LOGIN -> FAILED");
 }
-main();
